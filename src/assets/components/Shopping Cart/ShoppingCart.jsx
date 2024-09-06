8dd9be1d828f7faa05875bb0b5f3c3e2f08d59d8
James Newton <blackslate@lexogram.com>
2024-09-06 13:06:50 +0300

    Show products in Shopping Cart
    ShoppingCart.jsx
    ---
    - Stop fetching and using data from data.json
    - Stop using useEffect() to update products
    
    * Import CartContext
    * Read `state` array from CartContext
    * Remove calculation of productList from the `return`
      statement
    * Replace `product` with `state` everywhere its used
    * Use precalculated `productList` in `return` statement
    
    CartContext.jsx
    ---
    * Use indirect (non-default) export for both
      CartContext and CartProvider
    
    useAddToCart.js
    ---
    * Use indirect import for CartContext

--- src/assets/components/Shopping Cart/ShoppingCart.jsx
+++ src/assets/components/Shopping Cart/ShoppingCart.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useRemoveFromCart } from "../../hooks/useRemoveFromCart";
-import { useFetch } from "../../hooks/useFetch";
+// import { useFetch } from "../../hooks/useFetch";
+import {CartContext} from "../../contexts/CartContext";
 
 function ShoppingCart() {
-  const { data } = useFetch("./data.json");
-  const [product, setProducts] = useState([]); // keep the products in the state
+  const { state } = useContext(CartContext)
+  console.log("state:", state);
+  
+  // const { data } = useFetch("./data.json");
+  // const [product, setProducts] = useState([]); // keep the products in the state
   const removeFromCart = useRemoveFromCart();
   // const clearCart = useClearCart();
 
   // when the data comes, update the state
-  useEffect(() => {
-    if (data && data.length > 0) {
-      setProducts(data); // Verileri state'e ata
-    }
-    console.log("Fetched data:", data);
-  }, []);
+  // useEffect(() => {
+  //   if (data && data.length > 0) {
+  //     setProducts(data); // Verileri state'e ata
+  //   }
+  //   console.log("Fetched data:", data);
+  // }, []);
 
   const handleQuantityChange = (productId, value) => {
     const numericValue = parseInt(value, 10); // convert the value to a number
@@ -27,47 +31,51 @@ function ShoppingCart() {
     setProducts(updatedCartArr);
   };
 
-  const totalPrices = product.reduce(
+  const totalPrices = state.reduce(
     (acc, item) => acc + (item.price || 0) * (item.quantity || 0), // add the price of each product to the accumulator
     0
   );
 
+  const productList = state.map((product) => (
+    <div key={product.id}>
+      <img
+        src={product.image}
+        alt={product.product_name}
+        className="shopping-image"
+      />
+      <div className="shopping-details">
+        <h2>{product.product_name}</h2>
+        <p>{product.bulk}</p>
+        <input
+          min="0"
+          type="number"
+          value={product.quantity || 0}
+          onChange={(e) =>
+            handleQuantityChange(product.id, e.target.value)
+          }
+        />
+        <p>{(product.price || 0).toFixed(2)}</p>
+        <p>
+          {((product.price || 0) * (product.quantity || 0)).toFixed(2)}
+        </p>
+        {/* NaN'ı önlemek için */}
+        <button onClick={() => removeFromCart(product.id)}>
+          <i className="fa-solid fa-trash"></i>
+        </button>
+        {/*  <button onClick={clearCart}>Clear Shoppin Cart</button> */}
+      </div>
+    </div>
+  ))
+
+  
+
   return (
     <>
       <div className="shopping-card">
-        {product.length === 0 ? (
+        {state.length === 0 ? (
           <h2>Shopping Cart is Empty</h2> // if it is empty
         ) : (
-          product.map((product) => (
-            <div key={product.id}>
-              <img
-                src={product.image}
-                alt={product.product_name}
-                className="shopping-image"
-              />
-              <div className="shopping-details">
-                <h2>{product.product_name}</h2>
-                <p>{product.bulk}</p>
-                <input
-                  min="0"
-                  type="number"
-                  value={product.quantity || 0}
-                  onChange={(e) =>
-                    handleQuantityChange(product.id, e.target.value)
-                  }
-                />
-                <p>{(product.price || 0).toFixed(2)}</p>
-                <p>
-                  {((product.price || 0) * (product.quantity || 0)).toFixed(2)}
-                </p>
-                {/* NaN'ı önlemek için */}
-                <button onClick={() => removeFromCart(product.id)}>
-                  <i className="fa-solid fa-trash"></i>
-                </button>
-                {/*  <button onClick={clearCart}>Clear Shoppin Cart</button> */}
-              </div>
-            </div>
-          ))
+          productList
         )}
       </div>
       <div>
